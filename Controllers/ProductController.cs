34960104191e1fffed522df445b21a803a559412
TuanTV37 <tuantv37@fpt.com.vn>
2022-07-22 15:57:23 +0700

    Tach layout user

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrganicWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SingleProduct()
+        {
+            return View();
+        }
+    }
+}
